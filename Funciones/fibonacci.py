65bd363b17fc375b24c68cce5424da5f250f5636
MiltonLab <1000ton.lab@gmail.com>
2018-03-16 09:01:03 -0500

    Create fibonacci.py

---
+++ Funciones/fibonacci.py
@@ -0,0 +1,13 @@
+'''
+Tutorial PyAr http://docs.python.org.ar/tutorial/3/index.html
+'''
+def fib(n):  # escribe la serie de Fibonacci hasta n
+    """Escribe la serie de Fibonacci hasta n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end = ', ')
+        a, b = b, a + b
+    print()
+      
+# Ahora llamamos a la funcion que acabamos de definir:
+fib(2000)
